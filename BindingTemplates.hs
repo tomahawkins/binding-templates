c4b8bdd084cb8ee099bf5b0eabe6783dbbf36566
Tom Hawkins <t@groq.com>
2022-06-15 10:36:35 -0700

    Refactored out mount point placement control with cleanup.

--- BindingTemplates.hs
+++ BindingTemplates.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
 
 
 module Main
@@ -17,90 +16,14 @@ import qualified Data.Text                     as T
 import           System.Directory               ( createDirectoryIfMissing )
 
 
--- | Generate binding templates for pivot, spx, and shift bindings over a range of BSLs.
-main :: IO ()
-main = do
-  generateTemplateLibrary
-  customExample
-
-
--- | An example of a custom placement for a remount.
-customExample :: IO ()
-customExample = writeFile "custom-example.svg" $ unpack $ svg $ template
-  [ PlaceToe lookToe 334 0  -- Remounted with Look Rockerace 15 back 4 mm
-  , PlaceHeel rockeraceHeel 334 0  -- to maximize hole separation.
-  , PlaceHeel pivotHeel     334 0  -- to maximize hole separation.
-  ]
-
-
--- | Generate all templates over a range of BSLs with a mount point of 0.
-generateTemplateLibrary :: IO ()
-generateTemplateLibrary = do
-
-  -- Alpine bindings.
-  forM_ [250 .. 340 :: Int] $ \bsl ->
-    forM_ templateLibrary $ \(name, toe, heel) -> do
-      createDirectoryIfMissing False $ unpack name
-      writeFile
-          (unpack name <> "/" <> unpack name <> "-bsl-" <> show bsl <> ".svg")
-        $ unpack
-        $ svg
-        $ template
-            [ PlaceToe toe (fromIntegral bsl) 0
-            , PlaceHeel heel (fromIntegral bsl) 0
-            ]
-
-  -- Alpine plate bindings.
-  writeFile "look-r22.svg" $ unpack $ svg $ template [PlacePlate r22 0]
-
-  -- Nordic bindings.
-  forM_ [36 .. 50] $ \euroSize -> do
-    createDirectoryIfMissing False "rossignol-ifp"
-    writeFile ("rossignol-ifp/rossignol-ifp-euro-" <> show euroSize <> ".svg")
-      $ unpack
-      $ svg
-      $ template [PlacePlate (rossignolIFP euroSize) 0]
-
-
--- | Library of all alpine and telemark templates.
-templateLibrary :: [(Text, ToeBinding, HeelBinding)]
-templateLibrary =
-  [ ("look-pivot"    , lookToe   , pivotHeel)
-  , ("look-spx"      , lookToe   , spxHeel)
-  , ("look-r20"      , lookR20Toe, lookR20Heel)
-  , ("look-rockerace", lookToe   , rockeraceHeel)
-  , ("marker-royal"  , royalToe  , royalHeel)
-  , ("salomon-shift" , shiftToe  , shiftHeel)
-  , ("salomon-sth2"  , sth2Toe   , sth2Heel)
-  , ("salomon-warden", wardenToe , sth2Heel)
-  , ("tyrolia"       , tyroliaToe, tyroliaHeel)
-  , ("bishop-bmf-ntn", bmfNtnToe , noHeel)
-  ]
-
-
--- | Placement of either a binding or a plate.
-data Placement
-  = PlaceToe ToeBinding BSL MountPoint
-  | PlaceHeel HeelBinding BSL MountPoint
-  | PlacePlate Plate MountPoint
-
-
--- | Spec for the toe piece.  X axis is position of the toe edge of the boot.
-newtype ToeBinding = ToeBinding [Hole]
-
-
--- | Spec for the heel piece.  X axis is position of the heel edge of the boot.
-newtype HeelBinding = HeelBinding [Hole]
-
-
--- | Spec for a binding plate.
-newtype Plate = Plate [Hole]
-
-
--- | Coordinates for hole.
+-- | Hole type and position relative to reference line.
 data Hole
-  = Pair Double Double  -- ^ Pair of holes: width and distance from reference line.
-  | Center Double       -- ^ A single hole in the center, given distance from reference line.
+  = Pair Double Double    -- ^ Pair of holes with specified width and position.
+  | Center Double         -- ^ A single hole in the center with position.
+
+
+-- | A template is a list of hole positions.
+newtype Template = Template { holes :: [Hole] } deriving Semigroup
 
 
 -- | Coordinates for a point in mm.
@@ -108,152 +31,48 @@ type Point = (Double, Double)
 
 
 -- | Boot sole length in mm.
-type BSL = Double
+type Bsl = Double
 
 
--- | Mount point, + in front of the line, - behind the line, in mm.
-type MountPoint = Double
+-- | Shift forward or backward.
+class Shift a where
+  shift :: Double -> a -> a
 
 
--- | Look Pivot heel.
-pivotHeel :: HeelBinding
-pivotHeel = HeelBinding [Pair 21 82, Pair 29 (82 - 32)]
+instance Shift Hole where
+  shift amount h = case h of
+    Pair w y -> Pair w $ y + amount
+    Center y -> Center $ y + amount
 
 
--- | Look SPX heel.
-spxHeel :: HeelBinding
-spxHeel = HeelBinding [Pair 42 26, Pair 42 (26 - 105)]
+instance Shift Template where
+  shift amount t = Template $ shift amount <$> holes t
 
 
--- | Look Rockerace heel.
-rockeraceHeel :: HeelBinding
-rockeraceHeel = HeelBinding [Pair 42 59, Pair 42 (59 - 39)]
+-- | A drawing is collection of SVG elements.
+newtype Drawing = Drawing [Text] deriving (Semigroup, Monoid)
 
 
--- | Look toe.
-lookToe :: ToeBinding
-lookToe = ToeBinding [Pair 35 (-16.5), Pair 42 (-16.5 + 41.5)]
+-- | Place toe and heel pieces into one template based on Bsl.
+placeToeHeel :: Template -> Template -> Bsl -> Template
+placeToeHeel toe heel bsl = shift (bsl / 2) toe <> shift (-bsl / 2) heel
 
 
--- | Look R20 racing plate toe.
-lookR20Toe :: ToeBinding
-lookR20Toe = ToeBinding [Pair 37 (-(16.5 + 33)), Pair 35 (110 - (16.5 + 33))]
+-- | Draw a template.
+template :: Template -> Drawing
+template t = centerLines <> scalingRulers <> mconcat (hole <$> holes t)
 
 
--- | Look R20 racing plate heel.
-lookR20Heel :: HeelBinding
-lookR20Heel = HeelBinding [Pair 34 (47.5 + 26), Pair 36 (47.5 + 26 - 119)]
-
-
--- | Look R22 racing plate.
-r22 :: Plate
-r22 = Plate [Pair 12 164, Pair 35 99, Pair 35 (-52), Pair 35 (-171)]
-
-
--- | Rossignol nordic IFP.
-rossignolIFP :: Int -> Plate
-rossignolIFP euroSize = Plate
-  [ Center 37
-  , Pair 26 (-10)
-  , Center (-132)
-  , Center (-200 - offset * 13)
-  , Center (-235.5 - offset * 13)
-  ]
-
- where
-
-  offset :: Double
-  offset | euroSize >= 36 && euroSize <= 38 = 0
-         | euroSize >= 39 && euroSize <= 41 = 1
-         | euroSize >= 42 && euroSize <= 44 = 2
-         | euroSize >= 45 && euroSize <= 47 = 3
-         | euroSize >= 48 && euroSize <= 50 = 4
-         | otherwise = error $ "Unsupported euro size: " <> show euroSize
-
-
--- | Salomon Shift.
-shiftToe :: ToeBinding
-shiftToe = ToeBinding [Center (-20 + 65), Pair 30 (-20 - 70)]
-
-shiftHeel :: HeelBinding
-shiftHeel = HeelBinding [Pair 36 15, Pair 36 (15 - 68)]
-
-
--- | Salomon STH2.
-sth2Toe :: ToeBinding
-sth2Toe = ToeBinding [Pair 42 (-15 + 30), Pair 40 (-15)]
-
-sth2Heel :: HeelBinding
-sth2Heel = HeelBinding [Pair 32 28, Pair 32 (28 - 75)]
-
-
--- | Salomon Warden toe.
-wardenToe :: ToeBinding
-wardenToe = ToeBinding [Pair 40 (-15 + 65), Pair 40 (-15)]
-
-
--- | Marker Royal family (Jester, Griffon, Squire).
-royalToe :: ToeBinding
-royalToe = ToeBinding [Pair 36 (-12 + 31), Pair 36 (-12)]
-
-royalHeel :: HeelBinding
-royalHeel = HeelBinding [Pair 32 25, Pair 32 (25 - 80)]
-
-
--- | Tyrolia.
-tyroliaToe :: ToeBinding
-tyroliaToe = ToeBinding [Pair 40 (-15 + 55), Pair 40 (-15)]
-
-tyroliaHeel :: HeelBinding
-tyroliaHeel = HeelBinding [Pair 20 17, Pair 42.5 (17 - 95)]
-
-
--- | Bishop BMF for NTN.
-bmfNtnToe :: ToeBinding
-bmfNtnToe = ToeBinding
-  [ Pair 38 (-25)
-  , Pair 38 (-(25 + 38))
-  , Pair 38 (-(25 + 38 + 38))
-  , Pair 38 (-(25 + 38 + 38))
-  , Center (-(25 + 38 + 38))
-  , Center (-244)
-  , Center (-(244 + 38))
-  ]
-
-noHeel :: HeelBinding
-noHeel = HeelBinding []
-
-
--- | Generate a template for a set of binding placements.
-template :: [Placement] -> Drawing
-template placements =
-  centerLines <> scalingRulers <> mconcat (placement <$> placements)
-
-
--- | Draw the holes for a binding placement.
-placement :: Placement -> Drawing
-placement = \case
-
-  PlaceToe (ToeBinding holes) bsl mount ->
-    mconcat ((hole $ mount + bsl / 2) <$> holes)
-
-  PlaceHeel (HeelBinding holes) bsl mount ->
-    mconcat ((hole $ mount - bsl / 2) <$> holes)
-
-  PlacePlate (Plate holes) mount -> mconcat $ hole mount <$> holes
-
-
--- | Draw a hole with a y bias.
-hole :: Double -> Hole -> Drawing
-hole bias = \case
+-- | Draw a hole.
+hole :: Hole -> Drawing
+hole h = case h of
   Pair w y -> target' (w / 2) y <> target' (-w / 2) y
   Center y -> target' 0 y
 
  where
 
-  target' x y | y' >= 0   = target (pageCenter2 + x, baseToe - y')
-              | otherwise = target (pageCenter1 + x, baseHeel - y')
-    where y' = y + bias
+  target' x y | y >= 0    = target (pageCenter2 + x, baseToe - y)
+              | otherwise = target (pageCenter1 + x, baseHeel - y)
 
 
 -- | Draws X and Y centerling lines for the template,
@@ -317,10 +136,6 @@ baseToe = 255
 
 baseHeel :: Double
 baseHeel = 5
-
-
--- | A drawing is collection of SVG elements.
-newtype Drawing = Drawing [Text] deriving (Semigroup, Monoid)
 
 
 -- | Draws a line between two points.
@@ -419,3 +234,139 @@ tag open element attributes =
 -- | Show for Text.
 showT :: Show a => a -> Text
 showT = pack . show
+
+
+-- | Common Look toe.
+lookToe :: Template
+lookToe = Template [Pair 35 (-16.5), Pair 42 (-16.5 + 41.5)]
+
+
+-- | Look Pivot.
+pivot :: Bsl -> Template
+pivot = placeToeHeel lookToe $ Template [Pair 21 82, Pair 29 (82 - 32)]
+
+
+-- | Look SPX.
+spx :: Bsl -> Template
+spx = placeToeHeel lookToe $ Template [Pair 42 26, Pair 42 (26 - 105)]
+
+
+-- | Look Rockerace.
+rockerace :: Bsl -> Template
+rockerace = placeToeHeel lookToe $ Template [Pair 42 59, Pair 42 (59 - 39)]
+
+
+-- | Look R22 racing plate.
+r22 :: Template
+r22 = Template [Pair 12 164, Pair 35 99, Pair 35 (-52), Pair 35 (-171)]
+
+
+-- | Rossignol nordic IFP.
+rossignolIFP :: Int -> Template
+rossignolIFP euroSize = Template
+  [ Center 37
+  , Pair 26 (-10)
+  , Center (-132)
+  , Center (-200 - offset * 13)
+  , Center (-235.5 - offset * 13)
+  ]
+
+ where
+
+  offset :: Double
+  offset | euroSize >= 36 && euroSize <= 38 = 0
+         | euroSize >= 39 && euroSize <= 41 = 1
+         | euroSize >= 42 && euroSize <= 44 = 2
+         | euroSize >= 45 && euroSize <= 47 = 3
+         | euroSize >= 48 && euroSize <= 50 = 4
+         | otherwise = error $ "Unsupported euro size: " <> show euroSize
+
+
+-- | Salomon Shift.
+shift' :: Bsl -> Template
+shift' = placeToeHeel (Template [Center (-20 + 65), Pair 30 (-20 - 70)])
+                      (Template [Pair 36 15, Pair 36 (15 - 68)])
+
+
+-- | Salomon STH2.
+sth2Heel :: Template
+sth2Heel = Template [Pair 32 28, Pair 32 (28 - 75)]
+
+
+sth2 :: Bsl -> Template
+sth2 = placeToeHeel (Template [Pair 42 (-15 + 30), Pair 40 (-15)]) sth2Heel
+
+
+-- | Salomon Warden.
+warden :: Bsl -> Template
+warden = placeToeHeel (Template [Pair 40 (-15 + 65), Pair 40 (-15)]) sth2Heel
+
+
+-- | Marker Royal family (Jester, Griffon, Squire).
+royal :: Bsl -> Template
+royal = placeToeHeel (Template [Pair 36 (-12 + 31), Pair 36 (-12)])
+                     (Template [Pair 32 25, Pair 32 (25 - 80)])
+
+
+-- | Head Tyrolia.
+tyrolia :: Bsl -> Template
+tyrolia = placeToeHeel (Template [Pair 40 (-15 + 55), Pair 40 (-15)])
+                       (Template [Pair 20 17, Pair 42.5 (17 - 95)])
+
+
+-- | Bishop BMF for NTN.
+bmfNtn :: Bsl -> Template
+bmfNtn bsl = shift (bsl / 2) $ Template
+  [ Pair 38 (-25)
+  , Pair 38 (-(25 + 38))
+  , Pair 38 (-(25 + 38 + 38))
+  , Pair 38 (-(25 + 38 + 38))
+  , Center (-(25 + 38 + 38))
+  , Center (-244)
+  , Center (-(244 + 38))
+  ]
+
+
+-- | Library of all alpine and telemark templates.
+templateLibrary :: [(Text, Bsl -> Template)]
+templateLibrary =
+  [ ("look-pivot"    , pivot)
+  , ("look-spx"      , spx)
+  , ("look-rockerace", rockerace)
+  , ("salomon-shift" , shift')
+  , ("salomon-sth2"  , sth2)
+  , ("salomon-warden", warden)
+  , ("marker-royal"  , royal)
+  , ("tyrolia"       , tyrolia)
+  , ("bishop-bmf-ntn", bmfNtn)
+  ]
+
+
+-- | Generate binding templates.
+main :: IO ()
+main = do
+
+  -- Alpine bindings.
+  forM_ [250 .. 340 :: Int] $ \bsl -> forM_ templateLibrary $ \(name, t) -> do
+    createDirectoryIfMissing False $ unpack name
+    writeFile
+        (unpack name <> "/" <> unpack name <> "-bsl-" <> show bsl <> ".svg")
+      $ unpack
+      $ svg
+      $ template
+      $ t
+      $ fromIntegral bsl
+
+  -- Alpine plate bindings.
+  writeFile "look-r22.svg" $ unpack $ svg $ template r22
+
+  -- Nordic bindings.
+  forM_ [36 .. 50] $ \euroSize -> do
+    createDirectoryIfMissing False "rossignol-ifp"
+    writeFile ("rossignol-ifp/rossignol-ifp-euro-" <> show euroSize <> ".svg")
+      $ unpack
+      $ svg
+      $ template
+      $ rossignolIFP euroSize
+
+
